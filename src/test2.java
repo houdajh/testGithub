8abb818631c8600d6b330f645c9df2cfb0385889
houdajh <jouharhouda2001@gmail.com>
2022-11-28 23:23:40 +0100

    master2

--- src/test2.java
+++ src/test2.java
@@ -1,9 +1,4 @@
 public class test2 extends test{
-<<<<<<< HEAD
-//I'm Bobby  :edit ik
-=======
-//I'm Bobby  : ikrame
->>>>>>> bf7005ea67131cbbe2426550e365bb1457ab137b
 
     protected  void getNamePhrase() {
         System.out.println("I'm Bobby");
