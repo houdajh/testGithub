844317bce8639468da3904e1a5ef3f3beec70c42
houdajh <jouharhouda2001@gmail.com>
2022-11-29 01:43:23 +0100

    resolving

--- src/test.java
+++ src/test.java
@@ -4,11 +4,6 @@ import java.util.ArrayList;
 import java. util.Arrays;
 import java.util.List;
 
-<<<<<<< HEAD
-//I don't have a name :edit hd
-=======
-//I don't have a name :edit houda
->>>>>>> bf7005ea67131cbbe2426550e365bb1457ab137b
 
 public class test {
     protected  void getNamePhrase() {
