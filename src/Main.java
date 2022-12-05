0c2122a4cfb1790f08c94d800cf2ef358604dcf1
ikramenouigua <ikrame.nouigua@gmail.com>
2022-12-05 23:48:46 +0100

    add changes

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,10 @@ public class Main {
         test animal = new test();
         animal.getNamePhrase();
         test animal2 = new test2();
+        //add animal7
+        test animal7 = new test2();
         animal2.getNamePhrase();
-<<<<<<< HEAD
-        //edit ikram
-=======
->>>>>>> 9e6f25646ca3ec91b59073678e522ad12b2dd89f
+
 
     }
 }
