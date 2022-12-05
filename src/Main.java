e6a9dc0f83e097ae9b145a0ce09f7fe5ad74f2d5
houdajh <jouharhouda2001@gmail.com>
2022-12-05 23:52:31 +0100

    remove edit

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,6 @@ public class Main {
         animal.getNamePhrase();
         test animal2 = new test2();
         animal2.getNamePhrase();
-<<<<<<< HEAD
-//hello test
-=======
-<<<<<<< HEAD
-        //edit ikram
-=======
->>>>>>> 9e6f25646ca3ec91b59073678e522ad12b2dd89f
-
->>>>>>> 9b564647cfe99842aadaa548312fa4cb8f36d1de
     }
 }
 
