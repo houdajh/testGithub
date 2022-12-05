14dfd00a0530ed099276ca3e2cf3cbebd504a223
ikramenouigua <ikrame.nouigua@gmail.com>
2022-12-06 00:04:54 +0100

    ikram chnage

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
         test animal = new test();
         animal.getNamePhrase();
         test animal2 = new test2();
+        test animal8 = new test2();
         animal2.getNamePhrase();
+        //ikram change code
 //edit main
     }
 }
