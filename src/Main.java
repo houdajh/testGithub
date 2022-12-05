cc2e0067c311e6ec5d5852594dff96a7797dab2e
houdajh <jouharhouda2001@gmail.com>
2022-12-06 00:01:27 +0100

    edit feature1

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,8 @@ public class Main {
         test animal = new test();
         animal.getNamePhrase();
         test animal2 = new test2();
-        //add animal7
-        test animal7 = new test2();
         animal2.getNamePhrase();
-<<<<<<< HEAD
-=======
-        
->>>>>>> cb4d848f2c8b4242e3c7fa4dd58d8cca844a87a9
+        //feature1
     }
 }
 
