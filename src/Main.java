bb6e6d7b6a01ba46e92ac12e3c220ff831b4070b
houdajh <jouharhouda2001@gmail.com>
2022-12-06 11:37:42 +0100

    edit

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,7 @@ public class Main {
         animal.getNamePhrase();
         test animal2 = new test2();
         animal2.getNamePhrase();
-<<<<<<< HEAD
-        //edit branch feature2
-
-
-=======
 //conflit feature1
->>>>>>> 028483bbcfa08658d8eb66db9dc2545225e0f134
     }
 }
 
